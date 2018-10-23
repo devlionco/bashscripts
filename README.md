f55f5719e909e0a268009de5d33ae273a8fa6165
kiril <kiril@devlion.co>
2018-10-23 11:58:25 +0300

    bash script to install LAMP on Ubuntu 16.04

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bashscripts
+# Devlion Company Common Bash Scripts  
 Common Bash Scripts of Devlion Company
+
+install_ubuntu_16_04_php_7_2.sh - bash script that will install LAMP on Ubuntu server 16.04
---
+++ install_ubuntu_16_04_php_7_2.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+sudo apt-get install git -y
+sudo apt-get install upgrade
+sudo apt-get install update
+#set Timezone
+sudo dpkg-reconfigure tzdata
+#INSTALL APACHE
+sudo apt-get install apache2 -y
+sudo a2enmod rewrite
+sudo service apache2 restart
+#INSTALL MYSQL
+sudo apt-get install mysql-server mysql-client -y
+#INSTALL PHP
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php7.2 -y
+sudo apt-get install php7.2-cli -y
+sudo apt-get install php7.2-mysql -y
+sudo apt-get install php7.2-curl -y
+sudo apt-get install libapache2-mod-php7.2 -y
+sudo apt-get install php7.2-zip -y
+sudo apt-get install php7.2-mbstring -y
+sudo apt-get install php7.2-opcache -y
+sudo apt-get install php7.2-intl -y
+sudo apt-get install php7.2-json -y
+sudo apt-get install php7.2-xs -y
+sudo apt-get install php7.2-gd -y
+sudo apt-get install php7.2-soap -y
+sudo apt-get install php7.2-xml -y
+sudo apt-get install php7.2-simplexml -y
+sudo apt-get install php7.2-spl -y
+sudo apt-get install php7.2-ldap -y
+sudo apt-get install php7.2-dev -y
+sudo apt-get install php7.2-xmlrpc -y
