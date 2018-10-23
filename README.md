8adf2b92ca73c800ffce09b62b84d1c268761909
kiril <kiril@devlion.co>
2018-10-23 12:01:10 +0300

    install unoconv extension on Ubunut server 18

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Common Bash Scripts of Devlion Company
 
 install_ubuntu_16_04_php_7_2.sh - bash script that will install LAMP on Ubuntu server 16.04
+install_unoconv_moodle_ubunut_18.sh - bash script to install unoconv extension on Ubunut server 18 
---
+++ install_unoconv_moodle_ubunut_18.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-get install python3-minimal -y
+sudo apt-get install graphviz -y
+sudo apt-get install ghostscript -y
+sudo apt-get install du -y
+sudo apt-get install aspell -y
+sudo apt-get install dot -y
+sudo apt-get install unoconv -y
