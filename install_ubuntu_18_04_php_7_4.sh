971dcbd5d34d59d9322405f0b63cbc61c09fe379
kiril <kiril@devlion.co>
2020-04-28 17:29:58 +0300

    Add instalations ubuntu php 7.4

---
+++ install_ubuntu_18_04_php_7_4.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+sudo apt-get install git -y
+sudo apt-get install upgrade
+sudo apt-get install update
+sudo apt-get install zip -y
+#set Timezone
+sudo dpkg-reconfigure tzdata
+
+#INSTALL APACHE
+sudo apt-get install apache2 -y
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+#INSTALL MYSQL
+sudo apt-get install mysql-server mysql-client -y
+
+#INSTALL PHP
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php7.4-curl -y
+sudo apt-get install php7.4-zip -y
+sudo apt-get install php7.4 -y
+sudo apt-get install php7.4-cli -y
+sudo apt-get install php7.4-mysql -y
+sudo apt-get install php7.4-curl -y
+sudo apt-get install libapache2-mod-php7.4 -y
+sudo apt-get install php7.4-zip -y
+sudo apt-get install php7.4-mbstring -y
+sudo apt-get install php7.4-opcache -y
+sudo apt-get install php7.4-intl -y
+sudo apt-get install php7.4-json -y
+sudo apt-get install php7.4-xs -y
+sudo apt-get install php7.4-gd -y
+sudo apt-get install php7.4-soap -y
+sudo apt-get install php7.4-xml -y
+sudo apt-get install php7.4-simplexml -y
+sudo apt-get install php7.4-spl -y
+sudo apt-get install php7.4-ldap -y
+sudo apt-get install php7.4-dev -y
+sudo apt-get install php7.4-xmlrpc -y
+
+sudo /etc/init.d/apache2 restart
